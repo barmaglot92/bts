2037a6884afbc3681a92735f935f86c1f7ee5970
Andrey Zhvakin <barmaglot@yandex-team.ru>
2020-02-07 23:12:48 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {Map} from "./components/Map/Map"
+import "./index.css"
+import styled from "styled-components"
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppWrapper>
+            <Map />
+        </AppWrapper>
+    )
 }
 
-export default App;
+const AppWrapper = styled.div`
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+`
+
+export default App
